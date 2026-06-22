c7785e0f34461b01f8883932296bab1497570313
vallistyc <ivaldhiohadisetyasa@gmail.com>
2026-06-22 13:38:16 +0700

    fix banner mitra

--- src/sct/Banner.jsx
+++ src/sct/Banner.jsx
@@ -4,11 +4,21 @@ import {
   CarouselContent,
   CarouselItem,
 } from "@/components/ui/carousel"
+import favorich from "../assets/favorich.jpg"
+import kmie from "../assets/kmie.jpg"
+import gagaldiet from "../assets/gagaldiet.jpg"
+import hota from "../assets/hota.jpg"
+import mieeco from "../assets/mieeco.jpg"
 
-const partners = ["Mitra 1", "Mitra 2", "Mitra 3", "Mitra 4", "Mitra 5"]
+const partners = [
+    {label:"Favorich",pic:favorich}, 
+    {label:"Denah K-Mie",pic:kmie}, 
+    {label:"Gagal Diet",pic:gagaldiet}, 
+    {label:"Hota",pic:hota}, 
+    {label:"Mie Eco",pic:mieeco}]
 
-const PartnerLogo = ({ label }) => (
-  <div className='bg-red-500 aspect-square w-full rounded-lg' aria-label={label} />
+const PartnerLogo = ({ label,pic }) => (
+  <img src={pic} alt={label} className='aspect-square w-full rounded-lg' aria-label={label} />
 )
 
 const Banner = () => {
@@ -16,15 +26,15 @@ const Banner = () => {
     <div className='flex flex-col h-fit bg-gray-900 px-4 sm:px-8 lg:px-10 py-5 gap-4 justify-center'>
         {/* Title */}
         <p className='font-outfit font-regular text-2xl lg:text-4xl text-white text-center'>
-            Mitra <span className='font-playfair font-bold italic'>Kami</span>
+            Mitra Terbaik <span className='font-playfair font-bold italic'>Kami</span>
         </p>
 
         {/* Logo UMKM */}
         <Carousel opts={{ align: "start" }} className='sm:hidden w-full'>
             <CarouselContent>
                 {partners.map((partner) => (
-                    <CarouselItem key={partner} className='basis-[42%]'>
-                        <PartnerLogo label={partner} />
+                    <CarouselItem key={partner.label} className='basis-[42%]'>
+                        <PartnerLogo pic={partner.pic} label={partner.label} />
                     </CarouselItem>
                 ))}
             </CarouselContent>
@@ -32,7 +42,7 @@ const Banner = () => {
 
         <div className='hidden sm:grid sm:grid-cols-3 lg:grid-cols-5 w-full gap-5 lg:gap-10'>
             {partners.map((partner) => (
-                <PartnerLogo key={partner} label={partner} />
+                <PartnerLogo key={partner.label} label={partner.label} pic={partner.pic} />
             ))}
         </div>
     </div>
